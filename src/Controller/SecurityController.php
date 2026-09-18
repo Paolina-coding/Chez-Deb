722a8219ea8b4bb47578cc13fce3c275292e316d
PaolinaA <paolina.info@gmail.com>
2026-09-18 14:10:54 +0200

    Basic account page and logout possible

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -13,6 +13,10 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
+        if ($this->getUser()) {
+            return $this->redirectToRoute('app_account');
+        }
+
         $error = $authenticationUtils->getLastAuthenticationError();
         $lastUsername = $authenticationUtils->getLastUsername();
 
@@ -26,6 +30,16 @@ class SecurityController extends AbstractController
         return $this->render('security/login.html.twig', [
             'form' => $form->createView(),
             'error' => $error,
+        ]);
+    }
+
+    #[Route('/account', name: 'app_account')]
+    public function account(): Response
+    {
+        $user = $this->getUser();
+
+        return $this->render('security/account.html.twig', [
+            'user' => $user,
         ]);
     }
 
